d5f6ff160122ed5d0329af52c888e66e372af612
Anaclet Ahishakiye <a.anaclet920@gmail.com>
2023-09-20 16:57:58 +0200

    login

--- routes/router-auth.js
+++ routes/router-auth.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router()
-const dotenv = require('dotenv')
+const bcrypt = require('bcrypt');
 
-
-
-
+const conn = require('../database');
 
 router.get('', (req, res) => {
-    res.render('auth/login')
+    res.render('auth/login', )
 })
 
 
@@ -16,17 +14,42 @@ router.get('/forgot-password', (req, res) => {
 })
 
 
-router.post('/post-login', async (req, res) => { 
-    
+router.post('/post-login', async (req, res) => {
+
     let username = req.body.email;
-	let password = req.body.password;
+    let password = req.body.password;
+    // let hashedPassword = await bcrypt.hash(password, 8); // this 8 is hash salt
 
-    let hashedPassword = await bcrypt.hash(password, 8)
+    conn.query("SELECT * FROM tbl_users WHERE email = ?", [username], async function (error, user, fields) {
+        if (error) throw error;
+        if (user.length > 0) {
+            let hash = user[0].password;
+            // Load hash from your password DB.
+            bcrypt.compare(password, hash).then(function (result) {
+                if (result == true) {
+                    console.log('Logged in!');
+                    request.session.loggedin = true;
+                    request.session.username = user[0];
+                    res.redirect('/');
+                } else {
+                    req.flash('error', 'Incorrect credentials provided!');
+                    console.log('invalid logins');
+                    res.redirect('/auth')
+                }
+            });
+        } else {
+            req.flash('error', 'Incorrect credentials provided!')
+            res.redirect('/auth')
+        }
+        res.end();
+    });
 
-    
 })
 
 
+router.get('/logout', (req, res) => {
+    req.session.destroy()
+})
 
 module.exports = router
 
--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require("express")
-const path = require('path')
-
-
+const session = require('express-session');
+const path = require('path');
+const flash = require('connect-flash');
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(session({
+	secret: 'This a  secret for session encoding',
+	resave: true,
+	saveUninitialized: true
+}));
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs') //set views engine
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //allow form submit and get values
+app.use(flash());
 
-const authRouter = require('./routes/router-auth')
+const authRouter = require('./routes/router-auth');
+
 app.use('/auth', authRouter)
+
+
+app.get('/', (req, res) =>{
+    if (req.session.loggedin) {
+        req.redirect('/home');
+    }else{
+        req.session.destroy();
+        res.redirect('/auth');
+    }
+});
 
 app.listen(3000, () => console.log('App is listening via post 3000'))
