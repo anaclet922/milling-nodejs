005ae24e318e4e6335c9b848d1e2fec3021066d0
Anaclet Ahishakiye <a.anaclet920@gmail.com>
2023-09-25 07:58:38 +0200

    sql file

--- milling_factory.sql
+++ milling_factory.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Sep 05, 2023 at 06:31 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 7.4.33
+-- Host: 127.0.0.1
+-- Generation Time: Sep 25, 2023 at 07:57 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,134 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_activities`
+--
+
+CREATE TABLE `tbl_activities` (
+  `id` int(11) NOT NULL,
+  `details` text NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `reminder_time` datetime NOT NULL,
+  `priority` enum('HIGH','MEDIUM','LOW') NOT NULL,
+  `assignee` int(11) NOT NULL,
+  `color` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_bills`
+--
+
+CREATE TABLE `tbl_bills` (
+  `id` int(11) NOT NULL,
+  `billy_type` varchar(255) NOT NULL,
+  `amount` double NOT NULL,
+  `month_paid` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `note` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_configs`
+--
+
+CREATE TABLE `tbl_configs` (
+  `id` int(11) NOT NULL,
+  `config_key` varchar(50) NOT NULL,
+  `value` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_configs`
+--
+
+INSERT INTO `tbl_configs` (`id`, `config_key`, `value`, `created_at`, `updated_at`) VALUES
+(1, 'system_name', 'MILLING FACTORY', '2022-04-14 06:04:13', '2023-09-16 15:15:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_customers`
+--
+
+CREATE TABLE `tbl_customers` (
+  `id` int(11) NOT NULL,
+  `id_no` varchar(100) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `about` text NOT NULL,
+  `location` varchar(512) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_debts`
+--
+
+CREATE TABLE `tbl_debts` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `sale_id` int(11) NOT NULL,
+  `debited_amount` double NOT NULL,
+  `note` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_departments`
+--
+
+CREATE TABLE `tbl_departments` (
+  `id` int(11) NOT NULL,
+  `title` varchar(512) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_departments`
+--
+
+INSERT INTO `tbl_departments` (`id`, `title`, `created_at`, `updated_at`) VALUES
+(2, 'Finance', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'Accounting', '2023-09-17 03:53:04', '2023-09-17 03:53:04'),
+(4, 'Sales and Marketing', '2023-09-17 04:51:51', '2023-09-17 04:51:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_expenses`
+--
+
+CREATE TABLE `tbl_expenses` (
+  `id` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `amount` double NOT NULL,
+  `sender` int(11) NOT NULL,
+  `note` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_forgot_password`
 --
 
@@ -35,7 +163,7 @@ CREATE TABLE `tbl_forgot_password` (
   `active` varchar(1) NOT NULL DEFAULT 'Y',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -52,7 +180,7 @@ CREATE TABLE `tbl_inventory` (
   `note` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -68,7 +196,50 @@ CREATE TABLE `tbl_milling` (
   `output_kg` double NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_payments`
+--
+
+CREATE TABLE `tbl_payments` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `sale_id` int(11) NOT NULL,
+  `amount_paid` double NOT NULL,
+  `method_of_payment` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `note` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_payments_methods`
+--
+
+CREATE TABLE `tbl_payments_methods` (
+  `id` int(11) NOT NULL,
+  `name` varchar(512) NOT NULL,
+  `status` enum('ACTIVE','INACTIVE') NOT NULL DEFAULT 'ACTIVE',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_payments_methods`
+--
+
+INSERT INTO `tbl_payments_methods` (`id`, `name`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Mobile Money', 'ACTIVE', '2023-09-23 07:39:02', '2023-09-23 10:49:24'),
+(2, 'Cheque', 'ACTIVE', '2023-09-23 07:39:28', '2023-09-23 08:51:10'),
+(4, 'Credit/Debit Card	', 'ACTIVE', '2023-09-23 10:56:20', '2023-09-23 10:57:30'),
+(5, 'Bank Transfer', 'ACTIVE', '2023-09-23 10:56:45', '2023-09-23 10:56:45'),
+(6, 'Debt', 'ACTIVE', '2023-09-23 10:57:27', '2023-09-23 10:57:27');
 
 -- --------------------------------------------------------
 
@@ -89,7 +260,7 @@ CREATE TABLE `tbl_purchases` (
   `seller` varchar(512) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -113,7 +284,7 @@ CREATE TABLE `tbl_sales` (
   `note` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -131,7 +302,57 @@ CREATE TABLE `tbl_users` (
   `active` varchar(1) NOT NULL DEFAULT 'Y',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `active`, `created_at`, `updated_at`) VALUES
+(1, 'Anaclet', 'Ahishakiye', 'anaclet', 'anaclet@example.com', '$2b$08$K5P4ZJMmW0zjPnHLxy9eyOmOu3LrM5vVwMDNWifU8R2NXLRxgOkGa', 'Y', '2023-09-17 01:35:18', '2023-09-22 09:09:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_vehicles`
+--
+
+CREATE TABLE `tbl_vehicles` (
+  `id` int(11) NOT NULL,
+  `plate_number` varchar(50) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `driver_id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `capacity` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_vehicles`
+--
+
+INSERT INTO `tbl_vehicles` (`id`, `plate_number`, `owner`, `driver_id`, `type`, `capacity`, `created_at`, `updated_at`) VALUES
+(1, 'RAA 252 A', 'Kalisa Doe', 0, 'SUV', '6 Persons', '2023-09-23 10:37:24', '2023-09-23 11:09:37'),
+(3, 'RAA 253 A', 'John DOe', 0, 'Van', '8 Persons', '2023-09-23 11:09:59', '2023-09-23 11:09:59'),
+(5, 'RAA 2 A', 'Kalisa Doe', 0, 'Ranger', '2 Tones', '2023-09-23 11:23:38', '2023-09-23 11:24:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_vehicles_report`
+--
+
+CREATE TABLE `tbl_vehicles_report` (
+  `id` int(11) NOT NULL,
+  `vehicle_id` int(11) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `expense_amount` double NOT NULL,
+  `expense_receiver` varchar(255) NOT NULL,
+  `note` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -146,7 +367,7 @@ CREATE TABLE `tbl_warehouse` (
   `action` enum('IN','OUT') NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -172,11 +393,53 @@ CREATE TABLE `tbl_workforce` (
   `password` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_activities`
+--
+ALTER TABLE `tbl_activities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_bills`
+--
+ALTER TABLE `tbl_bills`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_configs`
+--
+ALTER TABLE `tbl_configs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_customers`
+--
+ALTER TABLE `tbl_customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_debts`
+--
+ALTER TABLE `tbl_debts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_departments`
+--
+ALTER TABLE `tbl_departments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_expenses`
+--
+ALTER TABLE `tbl_expenses`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_forgot_password`
@@ -194,6 +457,18 @@ ALTER TABLE `tbl_inventory`
 -- Indexes for table `tbl_milling`
 --
 ALTER TABLE `tbl_milling`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_payments`
+--
+ALTER TABLE `tbl_payments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_payments_methods`
+--
+ALTER TABLE `tbl_payments_methods`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -215,6 +490,18 @@ ALTER TABLE `tbl_users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_vehicles`
+--
+ALTER TABLE `tbl_vehicles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_vehicles_report`
+--
+ALTER TABLE `tbl_vehicles_report`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_warehouse`
 --
 ALTER TABLE `tbl_warehouse`
@@ -229,6 +516,48 @@ ALTER TABLE `tbl_workforce`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `tbl_activities`
+--
+ALTER TABLE `tbl_activities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_bills`
+--
+ALTER TABLE `tbl_bills`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_configs`
+--
+ALTER TABLE `tbl_configs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `tbl_customers`
+--
+ALTER TABLE `tbl_customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_debts`
+--
+ALTER TABLE `tbl_debts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_departments`
+--
+ALTER TABLE `tbl_departments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_expenses`
+--
+ALTER TABLE `tbl_expenses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_forgot_password`
@@ -249,6 +578,18 @@ ALTER TABLE `tbl_milling`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `tbl_payments`
+--
+ALTER TABLE `tbl_payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_payments_methods`
+--
+ALTER TABLE `tbl_payments_methods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `tbl_purchases`
 --
 ALTER TABLE `tbl_purchases`
@@ -264,6 +605,18 @@ ALTER TABLE `tbl_sales`
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_vehicles`
+--
+ALTER TABLE `tbl_vehicles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbl_vehicles_report`
+--
+ALTER TABLE `tbl_vehicles_report`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
